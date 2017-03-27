507ebf7296c35d9b1a10e0c86aa7d1d728641337
Meatloose <poiuy_1100@163.com>
2017-03-27 13:13:15 +0800

    [Stores] rename to indicator

--- src/stores/modules.js
+++ src/stores/modules.js
@@ -1,7 +1,7 @@
 import Popup from 'stores/popup/store'
 import Loading from 'stores/loading/store'
 import Toast from 'stores/toast/store'
-import Indicator from 'stores/loading-indicator/store'
+import Indicator from 'stores/indicator/store'
 import NativeAPI from 'stores/native-api/store'
 
 export default {
